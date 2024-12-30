075cd291ca68b147681c2e255cc68faa91c6e442
Emile Drablant <emiledrablant@proton.me>
2024-12-30 12:06:53 -0300

    Added basic fetching of weather

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 
 import "./style.css"
+
+async function fetchWeather(location) {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=C5EWCRR372R86ZHCG4F5S4SE4`, {mode: 'cors'});
+    return await response.json();
+}
+
+console.log(fetchWeather("Cayenne"));
